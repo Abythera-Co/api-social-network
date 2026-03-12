cb7ade665cfb7fdae05f33832086ac2e9ae76182
Angelo Miguel Requenha <angelorequenha@gmail.com>
2026-03-12 13:09:09 -0300

    feat: A new endpoint has been created to retrieve the community by name.

--- src/main/java/com/network/apiSocialNetwork/controller/CommunityController.java
+++ src/main/java/com/network/apiSocialNetwork/controller/CommunityController.java
@@ -1,16 +1,20 @@
 package com.network.apiSocialNetwork.controller;
 
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Pageable;
 
 import com.network.apiSocialNetwork.entity.Community;
 import com.network.apiSocialNetwork.service.CommunityService;
 import com.network.apiSocialNetwork.dto.CommunityRequestDTO;
+import com.network.apiSocialNetwork.dto.CommunityResponseDTO;
 
 import jakarta.validation.Valid;
 
 import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/community-controller")
@@ -26,9 +30,28 @@ public class CommunityController {
         return ResponseEntity.status(HttpStatus.CREATED).body(communityService.createCommunity(communityRequestDTO.getName()));
     }
 
+    @PostMapping("/get-community-by-name/{name}")
+    public ResponseEntity<CommunityResponseDTO> getCommunityByName(@PathVariable String name) {
+        List<Community> communities = communityService.getAllCommunities(Pageable.unpaged());
+        for (Community community : communities) {
+            if (community.getName().equalsIgnoreCase(name)) {
+                return ResponseEntity.ok(new CommunityResponseDTO(community.getId(), community.getName()));
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
     @GetMapping("/list-communities")
-    public ResponseEntity<List<Community>> listCommunities() {
-        return ResponseEntity.ok(communityService.getAllCommunities());
+    public ResponseEntity<List<CommunityResponseDTO>> listCommunities(@PageableDefault(size=10) Pageable pageable) {
+        List<Community> communities = communityService.getAllCommunities(pageable);
+        List<CommunityResponseDTO> response = new ArrayList<>();
+        for (Community community : communities) {
+            response.add(new CommunityResponseDTO(
+                community.getId(),
+                community.getName()
+            ));
+        }
+        return ResponseEntity.ok(response);
     }
     
     @DeleteMapping("/delete-community-by-id")
